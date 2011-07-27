0fa021c9ae45a8afd1b9a54eeb4dd9746b0cdf0b
Bruce Werdschinski <bwerdschinski@gmail.com>
2011-07-27 23:22:58 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rankify::Application.config.secret_token = '4ecac54e2f3ef32cd269bd09171b4449b34a9cdcb8dfda528f273e811914c6d187ec7767d8f37ea17e8143dda4a44060518117bc3fdab474a8ce43bc2d9a2570'
